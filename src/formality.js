974a187db5c62293c8b5cdd51abcaf0e61ddb6d7
Karsten Zhou <54686209+XiaoSong-CPE@users.noreply.github.com>
2024-05-11 02:31:57 +0000

    none

--- src/formality.js
+++ src/formality.js
@@ -1,13 +1,13 @@
 import fs from 'fs'
 import { parse } from 'csv-parse/browser/esm/sync'
 
-const dataRaw = await fs.readFileSync("D:\\文档\\GitHub\\paper\\src\\translation-pair.csv", 'utf8')
+const dataRaw = await fs.readFileSync('/workspaces/paper/src/translation-pair.csv', 'utf8')
 const data = parse(dataRaw, {
-	bom: true,
-	delimiter: ';',
-	columns: true,
-	skip_empty_lines: true
-  })
+  bom: true,
+  delimiter: ';',
+  columns: true,
+  skip_empty_lines: true
+})
 async function query(data) {
   const response = await fetch(
     'https://api-inference.huggingface.co/models/s-nlp/roberta-base-formality-ranker',
@@ -25,7 +25,9 @@ let results = []
 for (const item of data) {
   const formality = await query(item['English'])
   results.push({ ...item, formality })
-  console.log({ ...item, formality });
-  // wait for 100ms
-  await new Promise((resolve) => setTimeout(resolve, 100))
+  console.log(JSON.stringify({ ...item, formality }, null, 2))
+  // wait for 1000ms
+  await new Promise((resolve) => setTimeout(resolve, 1000))
 }
+// save results to a file
+fs.writeFileSync('/workspaces/paper/src/formality.json', JSON.stringify(results, null, 2))
